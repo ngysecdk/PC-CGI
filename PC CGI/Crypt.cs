f4c9d5d8e12c6b89a68697c8f9b59535ef7f5880
Ярослав Смирнов <d3156@mail.ru>
2021-10-10 23:17:49 +0300

    Добавьте файлы проекта.

---
+++ PC CGI/Crypt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PC_CGI
+{
+    class Cryptor
+    {
+        Cryptor()
+        {
+            //TODO Нужно Обменяься ключами с серером и поднять сеанс
+        }
+
+        byte[] Encrypt(byte[] data)
+        {
+            return data;
+        }
+
+        byte[] Decrypt(byte[] data)
+        {
+            return data;
+        }
+
+    }
+}
---
+++ PC CGI/DataDownload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//https://fooobar.com/questions/63815/how-to-convert-a-structure-to-a-byte-array-in-c
+
+namespace PC_CGI
+{
+    class DataDownload
+    {
+        DataDownload()
+        {
+            //Curve25519()
+            
+        }
+    }
+
+
+}
