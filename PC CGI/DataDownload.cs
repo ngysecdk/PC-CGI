0692af6bbc1bf943d5cc14479985724e2073a577
Ярослав Смирнов <d3156@mail.ru>
2021-10-12 05:20:51 +0300

    Добавлен экран загрузки и некоторый функционал

--- PC CGI/DataDownload.cs
+++ PC CGI/DataDownload.cs
@@ -1,6 +1,9 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
 //https://fooobar.com/questions/63815/how-to-convert-a-structure-to-a-byte-array-in-c
+
 
 namespace PC_CGI
 {
@@ -10,19 +13,30 @@ namespace PC_CGI
         Socket socket;
         static int port = 7755;              /// порт сервера
         static string address = "192.168.0.1"; /// адрес сервера
-        DataDownload()
+        public DataDownload()
         {
             //Curve25519()
             cryptor = new Cryptor();
-            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             // подключаемся к удаленному хосту
-            socket.Connect(ipPoint);
+            retry:
+            try { socket.Connect(IPAddress.Parse(address), port); }
+            catch (System.Exception e) { 
+                if (System.Windows.MessageBox.Show(e.Message + "\nПовторить попытку? (Не думаю. что это поможет)", 
+                    "Ошибка при подключении к серверу",
+                    System.Windows.MessageBoxButton.YesNo,
+                    System.Windows.MessageBoxImage.Question,
+                    System.Windows.MessageBoxResult.Yes,
+                    System.Windows.MessageBoxOptions.DefaultDesktopOnly) == System.Windows.MessageBoxResult.No)
+                    Application.Current.Shutdown(-1);
+                else goto retry;
+            }
+            
         }
 
         ~DataDownload()
         {
-            socket.Shutdown(SocketShutdown.Both);
+            if (socket.Connected) socket.Shutdown(SocketShutdown.Both);
             socket.Close();
         }
     }
